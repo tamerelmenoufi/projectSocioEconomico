d0969ba30a1d096d2a05ab7fde99c253943308e1
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-23 10:07:19 -0400

    Atualizações

--- src/usuarios/index.php
+++ src/usuarios/index.php
@@ -3,8 +3,19 @@
 ?>
 
 <div class="container">
-    <div class="row">
-        <h3>Usuários do Sistema</h3>
-
+    <div class="row mt-3">
+        <div class="d-flex justify-content-between">
+            <div class="p-10"><h3>Usuários do Sistema</h3></div>
+            <div class="p-2">
+                <button class="btn btn-primary">
+                    Novo
+                </button>
+            </div>
+        </div>
     </div>
+
+    <div class="row">
+        Dados do sistema atual
+    </div>
+
 </div>
