1d0ff855c0fb06cd900ebcda8d510c27a35d0fde
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-23 11:20:35 -0400

    Atualizações

--- src/usuarios/form.php
+++ src/usuarios/form.php
@@ -36,7 +36,7 @@
     }
 
 
-    $query = "select * from usuarios where codigo = '{$_GET['cod']}'";
+    $query = "select * from usuarios where codigo = '{$_POST['cod']}'";
     $result = mysqli_query($con, $query);
     $d = mysqli_fetch_object($result);
 ?>
