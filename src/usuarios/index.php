73b6900ac0ef45c2ae14d533c97bff31f0145bd3
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-23 11:19:26 -0400

    Atualizações

--- src/usuarios/index.php
+++ src/usuarios/index.php
@@ -69,6 +69,7 @@
             Carregando();
             $.ajax({
                 url:"src/usuarios/form.php",
+                type:"POST",
                 data:{
                     cod,
                 },
