778788f66afe89edb5ea5d8266530203c9f6ae06
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-31 15:40:31 -0400

    Atualizações

--- src/se/ef.php
+++ src/se/ef.php
@@ -63,8 +63,67 @@
         top:8px;
         z-index:0;
     }
+    .tab-content{
+        border-right:1px #ccc solid;
+        border-bottom:1px #ccc solid;
+        border-left:1px #ccc solid;
+    }
 </style>
 <h4 class="Titulo<?=$md5?>">Estrutura Familiar</h4>
+
+
+
+
+
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <button
+            class="nav-link active"
+            id="home-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#listaEstruturaFamiliar"
+            type="button"
+            role="tab"
+            aria-controls="listaEstruturaFamiliar"
+            aria-selected="true"
+    >Lista dos cadastros</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button
+            class="nav-link"
+            id="profile-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#formEstruturaFamiliar"
+            type="button"
+            role="tab"
+            aria-controls="formEstruturaFamiliar"
+            aria-selected="false"
+    >Formulário de Cadastro</button>
+  </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div
+        class="tab-pane fade show active"
+        id="listaEstruturaFamiliar"
+        role="tabpanel"
+        aria-labelledby="lista-tab"
+        tabindex="0"
+    >Lista dos cadastros</div>
+  <div
+        class="tab-pane fade"
+        id="formEstruturaFamiliar"
+        role="tabpanel"
+        aria-labelledby="form-tab"
+        tabindex="0"
+    >Formulários dos cadastros</div>
+</div>
+
+
+
+
+
+
+
     <form id="form-<?= $md5 ?>">
         <div class="row">
             <div class="col">
