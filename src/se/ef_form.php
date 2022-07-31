2fb8b03f0bdecaeaa57e3de1cd98a9b13b8b1555
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-31 16:43:42 -0400

    Atualizações

--- src/se/ef_form.php
+++ src/se/ef_form.php
@@ -168,7 +168,7 @@
         </div>
 
 
-        <div style="position:fixed; bottom:0; left:0; right:30px; height:50px; background-color:#fff; padding:0px;">
+        <div style="position:fixed; bottom:0; left:0; right:40px; height:50px; background-color:#fff; padding:0px;">
             <div style="display:flex; justify-content:end">
                 <button type="submit" SalvarFoto class="btn btn-success btn-ms">Salvar</button>
                 <input type="hidden" id="codigo" value="<?=$_POST['cod']?>" />
