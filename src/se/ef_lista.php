b4b9edcfa5d9d305757a9b291c42263fcbf3f781
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-31 18:03:35 -0400

    Atualizações

--- src/se/ef_lista.php
+++ src/se/ef_lista.php
@@ -8,7 +8,7 @@
 <h3>Aqui é a lista completa</h3>
 <?php
 
-$query = "select * from ef where se_codigo = '{$_SESSION['se_codigo']}'";
+echo $query = "select * from ef where se_codigo = '{$_SESSION['se_codigo']}'";
 $result = mysqli_query($con, $query);
 while($d = mysqli_fetch_object($result)){
 ?>
