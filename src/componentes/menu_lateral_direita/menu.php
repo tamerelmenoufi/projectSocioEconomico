6e8784855049b842032327bf5c9dcfaa8d710604
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-30 23:49:42 -0400

    Atualizações

--- src/componentes/menu_lateral_direita/menu.php
+++ src/componentes/menu_lateral_direita/menu.php
@@ -24,12 +24,20 @@ aria-controls="offcanvasDireita" -->
 
 <script>
 
-if (WURFL.is_mobile === true && WURFL.form_factor === "Smartphone") {
-    // moblie
+
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
     alert('mobile');
-} else {
-    //nao mobile
+  }
+ else {
     alert('pc');
-}
+  }
+
 
 </script>
