17ecc72fc9081587b65632484b370252cc0439bf
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-01 23:26:11 -0400

    Atualizações

--- lib/fn.php
+++ lib/fn.php
@@ -3,14 +3,20 @@
     function dataBr($dt){
         list($d, $h) = explode(" ",$dt);
         list($y,$m,$d) = explode("-",$d);
-        $data = "{$d}/{$m}/$y".(($h)?" {$h}":false);
+        $data = false;
+        if($y && $m && $d){
+            $data = "{$d}/{$m}/$y".(($h)?" {$h}":false);
+        }
         return $data;
     }
 
     function dataMysql($dt){
         list($d, $h) = explode(" ",$dt);
         list($d,$m,$y) = explode("/",$d);
-        $data = "{$y}-{$m}-$d".(($h)?" {$h}":false);
+        $data = false;
+        if($y && $m && $d){
+            $data = "{$y}-{$m}-$d".(($h)?" {$h}":false);
+        }
         return $data;
     }
 
