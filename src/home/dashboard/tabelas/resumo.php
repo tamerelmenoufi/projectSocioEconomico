05c046487518fa057a56f918029afc6f5c146af6
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-03 22:19:43 -0400

    Atualizações

--- src/home/dashboard/tabelas/resumo.php
+++ src/home/dashboard/tabelas/resumo.php
@@ -11,24 +11,41 @@
     $d = mysqli_fetch_object($result);
 
 ?>
+<style>
+    .painel{
+        width:80%;
+        height:90px;
+        background-color:blue;
+        border:solid 1px blue;
+        border-radius:9px;
+    }
+</style>
 <div class="row" style="margin:0; padding:0;">
-    <div class="col-md-3">
-        <h5>Total de Beneficiários</h5>
-        <h3><?=$d->total?></h3>
+    <div class="col-md-3 painel">
+        <div class="painel">
+            <h5>Total de Beneficiários</h5>
+            <h3><?=$d->total?></h3>
+        </div>
     </div>
 
-    <div class="col-md-3">
-        <h5>Pesquisas Iniciadas</h5>
-        <h3><?=$d->iniciadas?></h3>
+    <div class="col-md-3 painel">
+        <div class="painel">
+            <h5>Pesquisas Iniciadas</h5>
+            <h3><?=$d->iniciadas?></h3>
+        </div>
     </div>
 
-    <div class="col-md-3">
-        <h5>Pesquisas Pendentes</h5>
-        <h3><?=$d->pendentes?></h3>
+    <div class="col-md-3 painel">
+        <div class="painel">
+            <h5>Pesquisas Pendentes</h5>
+            <h3><?=$d->pendentes?></h3>
+        </div>
     </div>
 
-    <div class="col-md-3">
-        <h5>Pesquisas Concluídas</h5>
-        <h3><?=$d->concluidas?></h3>
+    <div class="col-md-3 painel">
+        <div class="painel">
+            <h5>Pesquisas Concluídas</h5>
+            <h3><?=$d->concluidas?></h3>
+        </div>
     </div>
 </div>
