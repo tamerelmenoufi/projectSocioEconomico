6b56623cabcc335aaa51d68f2749e56363385e11
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-03 22:41:58 -0400

    Atualizações

--- src/home/dashboard/graficos/municipios.php
+++ src/home/dashboard/graficos/municipios.php
@@ -32,30 +32,77 @@
 
 ?>
 
-    const TiposCtx<?=$md5?> = document.getElementById('Tipos<?=$md5?>');
+const TiposCtx<?=$md5?> = document.getElementById('Tipos<?=$md5?>');
 
-    const Tipos<?=$md5?> = new Chart(TiposCtx<?=$md5?>,
-        {
-            type: 'bar',
-            data: {
-                labels: [<?=$R?>],
-                datasets: [{
+const Tipos<?=$md5?> = new Chart(TiposCtx<?=$md5?>,
+    {
+        type: 'bar',
+        data: {
+            labels: [<?=$R?>],
+            datasets: [
+                {
                     label: [<?=$R?>],
                     data: [<?=$Q?>],
-                    backgroundColor: [
-                        'rgb(54, 162, 235, 0.2)',
-                        'rgb(201, 203, 207, 0.2)',
-                    ],
-                    borderColor: [
-                        'rgb(54, 162, 235, 1)',
-                        'rgb(201, 203, 207, 1)',
-                    ],
+                    backgroundColor: 'rgb(255, 159, 64, 0.2)',
+                    // [
+                    //     'rgba(255, 99, 132, 0.2)',
+                    //     'rgba(54, 162, 235, 0.2)',
+                    //     'rgba(255, 206, 86, 0.2)',
+                    // ],
+                    borderColor: 'rgb(255, 159, 64, 1)',
+                    // [
+                    //     'rgba(255, 99, 132, 1)',
+                    //     'rgba(54, 162, 235, 1)',
+                    //     'rgba(255, 206, 86, 1)',
+                    // ],
                     borderWidth: 1,
                     rotulos: [<?=$R?>]
-                }]
+                }
+        ]
+        },
+        options: {
+            indexAxis: 'y',
+            elements: {
+                bar: {
+                    borderWidth: 2,
+                }
+            },
+            responsive: true,
+            plugins: {
+                legend: false/*{
+    position: 'right',
+  }*/,
+                title: {
+                    display: false,
+                    text: 'Pesquisa de satisfação'
+                },
+
+
+                tooltip: {
+                    callbacks: {
+                        title: function (context) {
+                            indx = context[0].parsed.y;
+                            return context[0].dataset.rotulos[indx];
+                        },
+                        label: function (context) {
+                            indx = context.parsed.y;
+                            var label = ' ' + context.dataset.label[indx] || '';
+
+                            if (label) {
+                                label += ' : ';
+                            }
+                            if (context.parsed.y !== null) {
+                                label += context.parsed.x + ' Registro(s)';
+                            }
+                            return label;
+                        }
+                    }
+                }
+
             }
-        }
-    );
+        },
+    }
+);
 
 
 
