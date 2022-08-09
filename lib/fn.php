46ba397ac2488c2661938290c791063d584b4922
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-09 08:33:24 -0400

    Atualizações

--- lib/fn.php
+++ lib/fn.php
@@ -79,7 +79,7 @@
         $rotulo = $v['rotulo'];
         $dados = json_decode($v['dados']);
         // $lista[] = print_r($dados, true);
-        $lista[] = '<div class="mb-3" style="width:100%; float:none;"><label for="'.$campo.'"><b>'.$rotulo.'</b></label></div><div style="width:100%; float:none;">';
+        $lista[] = '<div class="mt-3" style="width:100%; float:none;"><label for="'.$campo.'"><b>'.$rotulo.'</b></label></div><div style="width:100%; float:none;">';
         for($i=0;$i<count($vetor);$i++){
             $lista[] = '  <div style="width:auto; margin-left:15px; float:left">
             <i class="fa-solid fa-square" style="color:#ccc"></i> '.$vetor[$i].'</div>';
