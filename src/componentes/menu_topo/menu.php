de035f3545fd1bcfbe3c5adeeeb9d8f635683d46
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-09 16:49:53 -0400

    Atualizações

--- src/componentes/menu_topo/menu.php
+++ src/componentes/menu_topo/menu.php
@@ -29,7 +29,7 @@
         <ul class="navbar-nav">
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  <i class="fa-solid fa-user"></i>
+                  <?=$_SESSION['ProjectSeLogin']->nome?> <i class="fa-solid fa-user"></i>
                 </a>
                 <ul class="dropdown-menu  dropdown-menu-end" aria-labelledby="navbarScrollingDropdown">
                     <li class="MenuLogin">
