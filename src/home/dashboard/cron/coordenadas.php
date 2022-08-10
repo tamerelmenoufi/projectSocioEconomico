901cb6a3c721eb348475c40c27c1cc9f17ea9941
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-10 09:31:45 -0400

    Atualizações

--- src/home/dashboard/cron/coordenadas.php
+++ src/home/dashboard/cron/coordenadas.php
@@ -1,9 +1,6 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/app/projectSocioEconomico/lib/includes.php");
 
-
-    echo "MD5:".$md5;
-
     if($_POST['lat'] and $_POST['lng'] and $_POST['codigo']){
         mysqli_query($con, "update municipios set coordenadas = '{$_POST['lat']}, {$_POST['lng']}' where codigo = '{$_POST['codigo']}'");
         exit();
