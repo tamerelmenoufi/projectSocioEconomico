b92fe19cc56ad28b38e07cdf263e6785ce9456ab
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-09 22:43:14 -0400

    Atualizações

--- src/home/dashboard/tabelas/resumo.php
+++ src/home/dashboard/tabelas/resumo.php
@@ -1,7 +1,7 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/app/projectSocioEconomico/lib/includes.php");
 
-    $query = "SELECT * FROM dashboard where grafico = 'graficos/resumo'";
+    $query = "SELECT * FROM dashboard where grafico = 'tabelas/resumo'";
     $result = mysqli_query($con, $query);
     $Rotulos = [];
     $Quantidade = [];
