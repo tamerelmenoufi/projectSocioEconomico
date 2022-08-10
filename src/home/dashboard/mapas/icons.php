437c9903e8fc31177d2a488cf836a6c5cafa2e28
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-10 00:27:05 -0400

    Atualizações

--- src/home/dashboard/mapas/icons.php
+++ src/home/dashboard/mapas/icons.php
@@ -2,27 +2,27 @@
 
     address =  `<?=$_POST['local']?>, Amazonas, Brasil`
 
-    geocoder<?=$_POST['md5']?>.geocode({ 'address':address, 'region': 'BR' }, (results, status) => {
+    geocoder<?=$_POST['md5'].$_POST['cod']?>.geocode({ 'address':address, 'region': 'BR' }, (results, status) => {
         console.log(address)
         console.log(status)
         console.log(google.maps.GeocoderStatus.OK)
 
         if (status == google.maps.GeocoderStatus.OK) {
-            // if (results[0] && !coordenadas<?=$_POST['md5']?>) {
+            // if (results[0] && !coordenadas<?=$_POST['md5'].$_POST['cod']?>) {
 
-                var latitude<?=$_POST['md5']?> = results[0].geometry.location.lat();
-                var longitude<?=$_POST['md5']?> = results[0].geometry.location.lng();
+                var latitude<?=$_POST['md5'].$_POST['cod']?> = results[0].geometry.location.lat();
+                var longitude<?=$_POST['md5'].$_POST['cod']?> = results[0].geometry.location.lng();
                 // console.log('Coordenadas:', local)
-                // console.log('Lat:'+latitude<?=$_POST['md5']?>)
-                // console.log('Lng:'+longitude<?=$_POST['md5']?>)
+                // console.log('Lat:'+latitude<?=$_POST['md5'].$_POST['cod']?>)
+                // console.log('Lng:'+longitude<?=$_POST['md5'].$_POST['cod']?>)
 
-                // var location<?=$_POST['md5']?> = new google.maps.LatLng(latitude<?=$_POST['md5']?>, longitude<?=$_POST['md5']?>);
-                // marker<?=$_POST['md5']?>.setPosition(location<?=$_POST['md5']?>);
-                // map<?=$_POST['md5']?>.setCenter(location<?=$_POST['md5']?>);
-                // map<?=$_POST['md5']?>.setZoom(18);
+                // var location<?=$_POST['md5'].$_POST['cod']?> = new google.maps.LatLng(latitude<?=$_POST['md5'].$_POST['cod']?>, longitude<?=$_POST['md5'].$_POST['cod']?>);
+                // marker<?=$_POST['md5'].$_POST['cod']?>.setPosition(location<?=$_POST['md5'].$_POST['cod']?>);
+                // map<?=$_POST['md5'].$_POST['cod']?>.setCenter(location<?=$_POST['md5'].$_POST['cod']?>);
+                // map<?=$_POST['md5'].$_POST['cod']?>.setZoom(18);
 
-                marker<?=$_POST['md5']?> = new google.maps.Marker({
-                    position: { lat: latitude<?=$_POST['md5']?>, lng: longitude<?=$_POST['md5']?> },
+                marker<?=$_POST['md5'].$_POST['cod']?> = new google.maps.Marker({
+                    position: { lat: latitude<?=$_POST['md5'].$_POST['cod']?>, lng: longitude<?=$_POST['md5'].$_POST['cod']?> },
                     map:map<?=$_POST['md5']?>,
                     title: "<?=$_POST['qt']?> Beneficiários em <?=$_POST['local']?>",
                     draggable:false,
