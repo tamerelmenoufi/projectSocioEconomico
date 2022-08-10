d946337c3aed972b758663e2951bd6df2ae2cf91
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-09 22:01:50 -0400

    Atualizações

---
+++ src/home/dashboard/cron/graficos.php
@@ -0,0 +1,218 @@
+<?php
+
+    include("{$_SERVER['DOCUMENT_ROOT']}/app/projectSocioEconomico/lib/includes.php");
+
+    $Values = [];
+
+    // RELATÓRIO "graficos/capital"
+    $grafico = 'graficos/capital';
+    $md5 = md5($grafico.$md5);
+
+    $query = "
+        SELECT
+            a.*,
+            count(*) as quantidade,
+            b.descricao as bairro
+        FROM se a
+        left join bairros_comunidades b on a.bairro_comunidade = b.codigo
+        where a.municipio = 66
+        group by a.bairro_comunidade
+        order by quantidade desc
+    ";
+    $result = mysqli_query($con, $query);
+    $Rotulos = [];
+    $Quantidade = [];
+    while($d = mysqli_fetch_object($result)){
+        set_time_limit(90);
+        $Rotulos[] = ($d->bairro);
+        $Quantidade[] = $d->quantidade;
+    }
+    $esquema = json_encode([
+                            'Rotulos' => $Rotulos,
+                            'Quantidade' => $Quantidade]
+                        );
+    $Values[] = "('{$grafico}','{$esquema}')";
+
+    // RELATÓRIO "graficos/municipios"
+    $grafico = 'graficos/municipios';
+    $md5 = md5($grafico.$md5);
+    $query = "
+    SELECT
+        a.*,
+        count(*) as quantidade,
+        b.municipio as municipio
+    FROM se a
+    left join municipios b on a.municipio = b.codigo
+    where a.municipio != 66
+    group by a.municipio
+    order by quantidade desc
+    ";
+    $result = mysqli_query($con, $query);
+    $Rotulos = [];
+    $Quantidade = [];
+    while($d = mysqli_fetch_object($result)){
+        set_time_limit(90);
+        $Rotulos[] = ($d->municipio);
+        $Quantidade[] = $d->quantidade;
+    }
+    $esquema = json_encode([
+        'Rotulos' => $Rotulos,
+        'Quantidade' => $Quantidade]
+    );
+    $Values[] = "('{$grafico}','{$esquema}')";
+
+
+
+    // RELATÓRIO "graficos/pesquisa"
+    $grafico = 'graficos/municipios';
+    $md5 = md5($grafico.$md5);
+    $query = "
+                select
+                    (select count(*) from se where percentual > 0 and percentual < 100) as iniciadas,
+                    (select count(*) from se where percentual = 0 ) as pendentes,
+                    (select count(*) from se where percentual = 100) as concluidas
+                from se limit 1
+    ";
+    $result = mysqli_query($con, $query);
+    $Rotulos = [];
+    $Quantidade = [];
+    while($d = mysqli_fetch_object($result)){
+        set_time_limit(90);
+        $Rotulos = ['Pendentes','Iniciadas','Concluídas'];
+        $Quantidade = [$d->pendentes, $d->iniciadas,$d->concluidas];
+    }
+    $esquema = json_encode([
+        'Rotulos' => $Rotulos,
+        'Quantidade' => $Quantidade]
+    );
+    $Values[] = "('{$grafico}','{$esquema}')";
+
+
+
+    // RELATÓRIO "graficos/rural"
+    $grafico = 'graficos/rural';
+    $md5 = md5($grafico.$md5);
+    $query = "
+                select
+                    (select count(*) from se where percentual > 0 and percentual < 100) as iniciadas,
+                    (select count(*) from se where percentual = 0 ) as pendentes,
+                    (select count(*) from se where percentual = 100) as concluidas
+                from se where local = 'rural' group by local
+    ";
+    $result = mysqli_query($con, $query);
+    $Rotulos = [];
+    $Quantidade = [];
+    while($d = mysqli_fetch_object($result)){
+        set_time_limit(90);
+        $Rotulos = ['Pendentes','Iniciadas','Concluídas'];
+        $Quantidade = [$d->pendentes, $d->iniciadas,$d->concluidas];
+    }
+    $esquema = json_encode([
+        'Rotulos' => $Rotulos,
+        'Quantidade' => $Quantidade]
+    );
+    $Values[] = "('{$grafico}','{$esquema}')";
+
+
+
+
+    // RELATÓRIO "graficos/urbana"
+    $grafico = 'graficos/urbana';
+    $md5 = md5($grafico.$md5);
+    $query = "
+                select
+                    (select count(*) from se where percentual > 0 and percentual < 100) as iniciadas,
+                    (select count(*) from se where percentual = 0 ) as pendentes,
+                    (select count(*) from se where percentual = 100) as concluidas
+                from se where local = 'urbano' group by local
+    ";
+    $result = mysqli_query($con, $query);
+    $Rotulos = [];
+    $Quantidade = [];
+    while($d = mysqli_fetch_object($result)){
+        set_time_limit(90);
+        $Rotulos = ['Pendentes','Iniciadas','Concluídas'];
+        $Quantidade = [$d->pendentes, $d->iniciadas,$d->concluidas];
+    }
+    $esquema = json_encode([
+        'Rotulos' => $Rotulos,
+        'Quantidade' => $Quantidade]
+    );
+    $Values[] = "('{$grafico}','{$esquema}')";
+
+
+
+    // RELATÓRIO "graficos/zona_geral"
+    $grafico = 'graficos/zona_geral';
+    $md5 = md5($grafico.$md5);
+    $query = "
+        select
+              *,
+              count(*) as quantidade
+        from se group by local order by quantidade desc
+    ";
+    $result = mysqli_query($con, $query);
+    $Rotulos = [];
+    $Quantidade = [];
+    while($d = mysqli_fetch_object($result)){
+        set_time_limit(90);
+        $Rotulos[] = $d->local;
+        $Quantidade[] = $d->quantidade;
+    }
+    $esquema = json_encode([
+        'Rotulos' => $Rotulos,
+        'Quantidade' => $Quantidade]
+    );
+    $Values[] = "('{$grafico}','{$esquema}')";
+
+
+
+
+    // RELATÓRIO "mapas/geral"
+    $grafico = 'mapas/geral';
+    $md5 = md5($grafico.$md5);
+    $query = "select
+                    a.*/*,
+                    (select count(*) from se where municipio = a.codigo) as qt*/
+                from municipios a
+            ";
+    $result = mysqli_query($con, $query);
+    $mun = [];
+    $qt = [];
+    while($d = mysqli_fetch_object($result)){
+        set_time_limit(90);
+        $mun[] = $d->municipio;
+        $qt[] = $d->qt;
+    }
+    $esquema = json_encode([
+        'Rotulos' => $Rotulos,
+        'Quantidade' => $Quantidade]
+    );
+    $Values[] = "('{$grafico}','{$esquema}')";
+
+
+
+    // RELATÓRIO "tabelas/resumo"
+    $grafico = 'tabelas/resumo';
+    $md5 = md5($grafico.$md5);
+    $query = "select
+        (select count(*) from se) as total,
+        (select count(*) from se where percentual > 0 and percentual < 100) as iniciadas,
+        (select count(*) from se where percentual = 0) as pendentes,
+        (select count(*) from se where percentual = 100) as concluidas
+    ";
+    $result = mysqli_query($con, $query);
+    $d = mysqli_fetch_object($result);
+
+    $Rotulos = ['Total','Iniciadas','Pendentes','Concluídas'];
+    $Quantidade = [$d->total, $d->iniciadas, $d->pendentes, $d->concluidas];
+
+    $esquema = json_encode([
+        'Rotulos' => $Rotulos,
+        'Quantidade' => $Quantidade]
+    );
+    $Values[] = "('{$grafico}','{$esquema}')";
+
+
+
+    echo $query = "INSERT INTO dashboard (grafico, esquema) VALUES ".implode(', ',$Values);
