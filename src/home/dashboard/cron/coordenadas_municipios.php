24ce77a54ec5b66324dcfdb5d8e4ed7baeffbfa8
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-10 11:31:05 -0400

    Atualizações

--- src/home/dashboard/cron/coordenadas_municipios.php
+++ src/home/dashboard/cron/coordenadas_municipios.php
@@ -104,6 +104,7 @@ $(function(){
                     // sleep(5000);
                     icones(enderecos[i], codigos[i])
                 }
+                // window.location.href='./coordenadas/comunidades.php';
         }, 5000);
 })
 
