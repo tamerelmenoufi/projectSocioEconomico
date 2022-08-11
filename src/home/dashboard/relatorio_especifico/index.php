70e3a6969f877295f517591270fd8c9830e27061
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-11 13:49:28 -0400

    Atualizações

--- src/home/dashboard/relatorio_especifico/index.php
+++ src/home/dashboard/relatorio_especifico/index.php
@@ -1,6 +1,9 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/app/projectSocioEconomico/lib/includes.php");
     // exit();
+
+    if(!$_SESSION['filtro_especifico']) exit();
+
 ?>
 
 <h5>Relatório Específico</h5>
