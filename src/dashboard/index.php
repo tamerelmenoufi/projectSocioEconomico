85a982c2cd6c910ba6a709bab6007d28d26bdfe2
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-17 00:07:45 -0400

    Atualizações

--- src/dashboard/index.php
+++ src/dashboard/index.php
@@ -4,6 +4,7 @@
 
     function array_multisum($arr){
         $sum = array_sum($arr);
+        echo key($child)."<br>";
         foreach($arr as $child) {
             $sum += is_array($child) ? array_multisum($child) : 0;
         }
