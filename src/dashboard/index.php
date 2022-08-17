4a6418ac014485db531d32bf4ae052947a1cab19
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-17 08:52:56 -0400

    Atualizações

--- src/dashboard/index.php
+++ src/dashboard/index.php
@@ -47,13 +47,13 @@
     }
 
     if($_SESSION['filtro_relatorio_municipio'] == 66){
-        print_r(array_indices($_SESSION['municipios']))."<hr>";
+        print_r(array_indices($_SESSION['municipios']['quantidade']))."<hr>";
         echo "Geral: de ".$_SESSION['filtro_relatorio_municipio'].': '.array_multisum($_SESSION['municipios']['quantidade'][$_SESSION['filtro_relatorio_municipio']])."<br>";
         foreach($_SESSION['municipios']['quantidade'][$_SESSION['filtro_relatorio_municipio']]['Urbano'] as $indice => $valores){
             echo "Geral: de ".$_SESSION['filtro_relatorio_municipio'].'Urbano - '.$indice.': '.array_multisum($_SESSION['municipios']['quantidade'][$_SESSION['filtro_relatorio_municipio']]['Urbano'][$indice])."<br>";
         }
     }else{
-        print_r(array_indices($_SESSION['municipios']))."<hr>";
+        print_r(array_indices($_SESSION['municipios']['quantidade']))."<hr>";
         echo "Geral: de ".$_SESSION['filtro_relatorio_municipio'].': '.array_multisum($_SESSION['municipios']['quantidade'][$_SESSION['filtro_relatorio_municipio']])."<br>";
         echo "Geral: de ".$_SESSION['filtro_relatorio_municipio'].'Urbano: '.array_multisum($_SESSION['municipios']['quantidade'][$_SESSION['filtro_relatorio_municipio']]['Urbano'])."<br>";
         echo "Geral: de ".$_SESSION['filtro_relatorio_municipio'].'Rural: '.array_multisum($_SESSION['municipios']['quantidade'][$_SESSION['filtro_relatorio_municipio']]['Rural'])."<br>";
