e1ddd36aeaa219d3238b47d5d3eac30f3d8b8351
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-17 00:02:00 -0400

    Atualizações

--- src/dashboard/index.php
+++ src/dashboard/index.php
@@ -31,7 +31,7 @@
 
     $result = mysqli_query($con,$query);
     while($d = mysqli_fetch_object($result)){
-        $_SESSION['municipios'][$st[$i]]['quantidade'][$d->cod_municipio][$d->local][$d->zona_urbana][$d->cod_bairro][$d->situacao] = $d->quantidade;
+        $_SESSION['municipios']['quantidade'][$d->cod_municipio][$d->local][$d->zona_urbana][$d->cod_bairro][$d->situacao] = $d->quantidade;
         $_SESSION['municipios']['nome'][$d->cod_municipio] = $d->municipio;
         $_SESSION['bairro']['nome'][$d->cod_bairro] = $d->descricao;
     }
