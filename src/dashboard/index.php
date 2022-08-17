3fe90f35cad8a27df3426583866d5d9e550fe2e6
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-17 09:38:27 -0400

    Atualizações

--- src/dashboard/index.php
+++ src/dashboard/index.php
@@ -33,7 +33,7 @@
 
     $result = mysqli_query($con,$query);
     while($d = mysqli_fetch_object($result)){
-        $_SESSION['municipios']['quantidade'][$d->cod_municipio][$d->cod_bairro][$d->local][$d->zona_urbana][$d->situacao] = $d->quantidade;
+        $_SESSION['municipios']['quantidade'][$d->cod_municipio][$d->local][$d->zona_urbana][$d->cod_bairro][$d->situacao] = $d->quantidade;
         $_SESSION['municipios']['nome'][$d->cod_municipio] = $d->municipio;
         $_SESSION['bairro']['nome'][$d->cod_bairro] = $d->descricao;
     }
@@ -41,12 +41,12 @@
     if($_SESSION['filtro_relatorio_municipio'] == 66){
         echo "Geral: de ".$_SESSION['filtro_relatorio_municipio'].': '.array_multisum($_SESSION['municipios']['quantidade'][$_SESSION['filtro_relatorio_municipio']])."<br>";
         foreach($_SESSION['municipios']['quantidade'][$_SESSION['filtro_relatorio_municipio']]['Urbano'] as $indice => $valores){
-            echo "Geral: de ".$_SESSION['filtro_relatorio_municipio'].'Urbano - '.$indice.': '.array_multisum($_SESSION['municipios']['quantidade'][$_SESSION['filtro_relatorio_municipio']]['Urbano'][$indice])."<br>";
+            echo "Geral: de ".$_SESSION['filtro_relatorio_municipio'].'Urbano - '.$indice.': '.array_multisum($_SESSION['municipios']['quantidade']['Urbano'][$indice][$_SESSION['filtro_relatorio_municipio']])."<br>";
         }
     }else{
         echo "Geral: de ".$_SESSION['filtro_relatorio_municipio'].': '.array_multisum($_SESSION['municipios']['quantidade'][$_SESSION['filtro_relatorio_municipio']])."<br>";
-        echo "Geral: de ".$_SESSION['filtro_relatorio_municipio'].'Urbano: '.array_multisum($_SESSION['municipios']['quantidade'][$_SESSION['filtro_relatorio_municipio']]['Urbano'])."<br>";
-        echo "Geral: de ".$_SESSION['filtro_relatorio_municipio'].'Rural: '.array_multisum($_SESSION['municipios']['quantidade'][$_SESSION['filtro_relatorio_municipio']]['Rural'])."<br>";
+        echo "Geral: de ".$_SESSION['filtro_relatorio_municipio'].'Urbano: '.array_multisum($_SESSION['municipios']['quantidade']['Urbano']['0'][$_SESSION['filtro_relatorio_municipio']])."<br>";
+        echo "Geral: de ".$_SESSION['filtro_relatorio_municipio'].'Rural: '.array_multisum($_SESSION['municipios']['quantidade']['Rural']['0'][$_SESSION['filtro_relatorio_municipio']])."<br>";
     }
 
     // foreach($_SESSION['bairro']['nome'] as $indice => $valor){
