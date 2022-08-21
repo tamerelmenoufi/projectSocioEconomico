1777e8094e2f78bf3ebaf27b6ad068c63b4d4c80
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-21 13:13:44 -0400

    Atualizações

--- src/dashboard/geral/grafico/geral_zona.php
+++ src/dashboard/geral/grafico/geral_zona.php
@@ -5,8 +5,26 @@
 
 
 <canvas id="Tipos<?= $md5 ?>" width="400" height="400"></canvas>
-
-<script>
+<ol class="list-group list-group-numbered">
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      Cras justo odio
+    </div>
+    <span class="badge bg-primary rounded-pill">14</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      Cras justo odio
+    </div>
+    <span class="badge bg-primary rounded-pill">14</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      Cras justo odio
+    </div>
+    <span class="badge bg-primary rounded-pill">14</span>
+  </li>
+</ol>
 
 
 <?php
