5bffed28d77e59acbcef7124a5e487a685a786ea
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-22 11:21:25 -0400

    Atualizações

--- src/dashboard/capital/mapa/capital.php
+++ src/dashboard/capital/mapa/capital.php
@@ -31,7 +31,7 @@
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         fullscreenControl: true,
         center: { lat: -3.142549, lng: -60.0141404 },
-        zoom: 11,
+        zoom: 10,
     });
 
     // marker<?=$md5?> = new google.maps.Marker({
