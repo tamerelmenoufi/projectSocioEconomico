07c52330b9f8512a818792553b7029923a323871
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-24 16:31:34 -0400

    Atualizações

--- src/dashboard/tabelas/geral.php
+++ src/dashboard/tabelas/geral.php
@@ -3,6 +3,14 @@
     // iniciados pendentes concluidos nao_encontrados
     // echo $_POST['filtro'] . ' & '.$_POST['opc'];
 
+    $titulo = [];
+
+    if($_SESSION['municipios']['nome'][$_SESSION['filtro_relatorio_municipio']] ){ $titulo[] = $_SESSION['municipios']['nome'][$_SESSION['filtro_relatorio_municipio']]; }
+    if($_SESSION['bairro']['nome'][$_SESSION['filtro_relatorio_bairro_comunidade']] ){ $titulo[] = $_SESSION['bairro']['nome'][$_SESSION['filtro_relatorio_bairro_comunidade']]; }
+    if($_SESSION['filtro_relatorio_tipo']) { $titulo[] = $_SESSION['filtro_relatorio_tipo']; }
+
+    if($titulo) $titulo = implode(" - ",$titulo);
+
     $fLocal = [];
     if($_SESSION['filtro_relatorio_municipio']) { $fLocal[] = " municipio = '{$_SESSION['filtro_relatorio_municipio']}'"; }
     if($_SESSION['filtro_relatorio_tipo']) { $fLocal[] = " local = '{$_SESSION['filtro_relatorio_tipo']}'"; }
@@ -41,6 +49,9 @@
     if(!$where) exit();
 ?>
 <div class="col">
+    <p>
+        Resultado da pesquisa: <?=$titulo?>
+    </p>
     <table class="table table-hover">
     <thead>
         <tr>
