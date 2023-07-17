a07f6cf86446b4f4bb2eaf8651c42287210ff530
tamerelmenoufi <tamer@mohatron.com.br>
2023-07-17 18:48:43 +0300

    Atualizações

---
+++ api/exportSql.php
@@ -0,0 +1,13 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/app/projectSocioEconomico/lib/includes.php");
+
+    $query = "SELECT * FROM `COLUMNS` where TABLE_SCHEMA = 'app' and COLUMN_NAME != 'codigo' order by TABLE_NAME";
+    $result = mysqli_query($con, $query);
+    while($d = mysqli_fetch_object($result)){
+        $Comando[$d->TABLE_NAME][] = $d->COLUMN_NAME;
+    }
+
+
+    foreach($Comando as $ind => $val){
+        echo $ind."<br>";
+    }
