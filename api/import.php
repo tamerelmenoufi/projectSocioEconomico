dc5d72720a1410dafa56685da1fc5fe22c5af86d
tamerelmenoufi <tamer@mohatron.com.br>
2023-07-20 18:37:50 +0300

    Atualizações

--- api/import.php
+++ api/import.php
@@ -1,7 +1,12 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/app/projectSocioEconomico/lib/includes.php");
 
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
+    $_POST = json_decode(file_get_contents('php://input'), true);
+    $dados = json_encode($_POST);
+
     if(!is_dir('logs')) mkdir('logs');
 
-    file_put_contents(date("YmdHis").".txt", print_r($_POST));
+    file_put_contents(date("YmdHis").".txt", print_r($dados, true));
 
