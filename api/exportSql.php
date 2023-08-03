75e46b2d511c67eafdc1e652ae7a99d9585417d7
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-03 10:05:26 -0500

    Atualizações

--- api/exportSql.php
+++ api/exportSql.php
@@ -1,7 +1,11 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/app/projectSocioEconomico/lib/includes.php");
 
-    $query = "SELECT * FROM `COLUMNS` where TABLE_SCHEMA = 'app' and COLUMN_NAME != 'codigo' and TABLE_NAME != 'zonas_manaus' order by TABLE_NAME";
+    $delTab = ['dashboard'];
+
+    $dataTable = ['usuarios']; 
+
+    $query = "SELECT * FROM `COLUMNS` where TABLE_SCHEMA = 'app' and COLUMN_NAME != 'codigo' and TABLE_NAME not in('".implode("','", $delTab)."') order by TABLE_NAME";
     $result = mysqli_query($conApi, $query);
     while($d = mysqli_fetch_object($result)){
         $Comando[$d->TABLE_NAME][] = $d->COLUMN_NAME;
@@ -21,15 +25,15 @@
         $Cmd[] = ['comando' => "DROP TABLE {$ind}"];
         $Cmd[] = ['comando' => $cmd];
 
-        $query = "select * from {$ind} limit 1000";
-        $result = mysqli_query($con, $query);
-        while($d = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-            $Cmd[] = ['comando' => "INSERT INTO $ind (codigo, ".implode(", ", $campos).") VALUES ('".implode("', '",$d)."')"];
+        if(in_array($ind, $dataTable)){
+            $query = "select * from {$ind} limit 1000";
+            $result = mysqli_query($con, $query);
+            while($d = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                $Cmd[] = ['comando' => "INSERT INTO $ind (codigo, ".implode(", ", $campos).") VALUES ('".implode("', '",$d)."')"];
+            }            
         }
 
     }
-
-
 
     echo json_encode($Cmd);
 
