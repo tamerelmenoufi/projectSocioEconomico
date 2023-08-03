a565c0f757e42f639a6c06f6d805d5771091fc02
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-03 13:32:07 -0500

    Atualizações

--- api/exportSqlPerfil.php
+++ api/exportSqlPerfil.php
@@ -2,7 +2,7 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/app/projectSocioEconomico/lib/includes.php");
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
-    $_POST = (file_get_contents('php://input'), true);
+    $_POST = file_get_contents('php://input');
 
 
     $Cmd[] = ['comando' => $_POST['perfil']];
