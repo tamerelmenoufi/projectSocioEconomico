f58fd68378996f7085bff6df5d8570c34f35aaf1
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-05 03:58:58 -0500

    Atualizações

---
+++ api/importEf.php.php
@@ -0,0 +1,13 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/app/projectSocioEconomico/lib/includes.php");
+
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
+    $_POST = json_decode(file_get_contents('php://input'), true);
+ 
+    if(!is_dir('logs')) mkdir('logs');
+
+    echo 'success';
+
+    file_put_contents('logs/ef-'.date("YmdHis").".txt", print_r($_POST, true));
+
