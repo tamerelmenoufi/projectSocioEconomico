3fea5104de029f78bf9b99f68917e75b3eb0ddff
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-07 07:38:44 -0500

    Atualizações

--- api/exportSql.php
+++ api/exportSql.php
@@ -1,6 +1,14 @@
 <?php
 
-    echo file_get_contents("https://mapcenso.project.tec.br/api/exportSql.php");
+
+$arrContextOptions=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+); 
+
+    echo file_get_contents("https://mapcenso.project.tec.br/api/exportSql.php", false, stream_context_create($arrContextOptions));
 
     exit();
 
