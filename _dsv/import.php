1d500fd90168135ecd4dfd381b535a1927c5042b
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-23 06:42:51 -0500

    Atualizações

--- _dsv/import.php
+++ _dsv/import.php
@@ -1,4 +1,5 @@
 <?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/app/projectSocioEconomico/lib/includes.php");
 
     $dados = file_get_contents("banco.csv");
 
@@ -6,6 +7,26 @@
 
     echo "<table border='1'>";
     foreach($linhas as $i => $col){
+
+        if($i == 0){
+            $cols = explode(";", $linhas[$i]);
+            $Campos = [];
+            foreach($cols as $j => $dado){
+                $Campos[] = "`campo1{$j}` VARCHAR(255) NOT NULL";
+            }   
+            $camando = "CREATE TABLE IF NOT EXISTS `se`.`tratar` ( ".implode(", ", $Campos)." )";
+            mysqli_query($con, $comando);       
+
+        }else{
+            $Dado = [];
+            $cols = explode(";", $linhas[$i]);
+            foreach($cols as $j => $dado){
+                set_time_limit(90);
+                $Dado[] = "'{$dado}'";
+            } 
+            $camando = "INSERT INTO `se`.`tratar` ( ".implode(", ", $Dado)." )";
+            mysqli_query($con, $comando);       
+        }
         set_time_limit(90);
         $cols = explode(";", $linhas[$i]);
         echo "<tr>";
@@ -16,7 +37,9 @@
             echo "</td>";
         }
         echo "</tr>";
+        if($i = 5) exit();
     }
+
     
 
     echo "</table>";
