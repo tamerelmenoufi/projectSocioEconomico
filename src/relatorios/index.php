7166fe692b69b2f566138f586c15872d17d27bad
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-24 07:13:40 -0500

    Atualizações

--- src/relatorios/index.php
+++ src/relatorios/index.php
@@ -8,59 +8,14 @@
         $_SESSION['relatorio']['data_final'] = $_POST['data_final'];
     }
 
-    $filtro = $f_usuario = $f_meta = $f_data = false;
-    if($_SESSION['relatorio']['usuario']){
-        $f_usuario = " and monitor_social in( {$_SESSION['relatorio']['usuario']} ) ";
-    }
-    if($_SESSION['relatorio']['meta']){
-        $f_meta = " and meta in( {$_SESSION['relatorio']['meta']} ) ";
-    }
-    if($_SESSION['relatorio']['data_inicial']){
-        $f_data = " and (data between '{$_SESSION['relatorio']['data_inicial']} 00:00:00' and '".(($_SESSION['relatorio']['data_final'])?:$_SESSION['relatorio']['data_inicial'])." 23:59:59')";
-    }
-
-    $filtro = $f_usuario . $f_meta . $f_data;
-
-
-    echo $query = "select count(*) from se where monitor_social > 0 and meta > 0 {$filtro} group by situacao";
 
 
 ?>
 <style>
-       .AreaDashboard{
+    .AreaDashboard{
         position:absolute;
         left:20px;
         right:20px;
-    }
-    .cartao{
-        position:relative;
-        width:99%;
-        min-height:120px;
-        background-color:#459adb;
-        border-radius:10px;
-        color:#fff;
-    }
-    .cartao span{
-        font-size:10px;
-        margin-left:10px;
-    }
-    .cartao p{
-        font-size:25px;
-        font-weight:bold;
-        text-align:center;
-        padding-top:0px;
-        padding-bottom:15px;
-    }
-    .cartao div{
-        position:absolute;
-        right:10px;
-        bottom:5px;
-        width:auto;
-        opacity:0;
-    }
-    .cartao div i{
-        cursor: pointer;
-        margin-left:10px;
     }
 </style>
 
@@ -115,84 +70,18 @@
 
 
 <div class="AreaDashboard">
-    <div class="row mb-3 mt-3">
-        <div class="col-md-1"></div>
-        <div class="col-md-10"><h3 style="color:#a1a1a1">Relatório Quantitativo</h3></div>
-        <div class="col-md-1"></div>
-    </div>
-    <div class="row">
-        <div class="col-md-1"></div>
-
-
-        <div class="col-md-2 mb-3">
-            <div class="cartao">
-                <span>Toatl Geral</span>
-                <p><?=$total?></p>
-                <div>
-                    <i acao='editar' filtro='' class="fa-solid fa-up-right-from-square"></i>
-                    <i acao='download' filtro='' class="fa-solid fa-file-arrow-down"></i>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-2 mb-3">
-            <div class="cartao">
-                <span>Pesquisas Iniciadas</span>
-                <p><?=$i?></p>
-                <div>
-                    <i acao='editar' filtro='i' class="fa-solid fa-up-right-from-square"></i>
-                    <i acao='download' filtro='i' class="fa-solid fa-file-arrow-down"></i>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-2 mb-3">
-            <div class="cartao">
-                <span>Pesquisas Pendentes</span>
-                <p><?=$p?></p>
-                <div>
-                    <i acao='editar' filtro='p' class="fa-solid fa-up-right-from-square"></i>
-                    <i acao='download' filtro='p' class="fa-solid fa-file-arrow-down"></i>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-2 mb-3">
-            <div class="cartao">
-                <span>Pesquisas Concluídas</span>
-                <p><?=$c?></p>
-                <div>
-                    <i acao='editar' filtro='c' class="fa-solid fa-up-right-from-square"></i>
-                    <i acao='download' filtro='c' class="fa-solid fa-file-arrow-down"></i>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-2 mb-3">
-            <div class="cartao">
-                <span>Beneficiários não encontrados</span>
-                <p><?=$n?></p>
-                <div>
-                    <i acao='editar' filtro='n' class="fa-solid fa-up-right-from-square"></i>
-                    <i acao='download' filtro='n' class="fa-solid fa-file-arrow-down"></i>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-1"></div>
-    </div>
+    <div geral></div>
 </div>
 
     <script>
         $(function(){
             Carregando('none')
-
-            $(".cartao").mouseover(function(){
-                $(this).children(".cartao div").css('opacity',1)
-            }).mouseout(function(){
-                $(this).children(".cartao div").css('opacity',0)
+            $.ajax({
+                url:"src/relatorios/telas/geral.php",
+                success:function(dados){
+                    $("div['geral']").html(dados);
+                }
             })
-
 
             $("#filtro_usuario").change(function(){
                 usuario = $(this).val();
