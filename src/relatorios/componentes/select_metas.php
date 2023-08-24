5dd5345117cbdb60b466f23f0ccd74d2dc0d3b4b
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-24 15:57:14 -0500

    Atualizações

--- src/relatorios/componentes/select_metas.php
+++ src/relatorios/componentes/select_metas.php
@@ -3,7 +3,7 @@
 ?>
 <option value="">::Todos as metas::</option>
 <?php
-$q = "select a.*, b.municipio as municipio_nome, c.descricao as bairro_nome from metas a left join municipios b on a.municipio = b.codigo left join bairros_comunidades c on a.bairro_comunidade = c.codigo where a.usuario = '{$_POST['usuario']}' order by municipio_nome, bairro_nome";
+$q = "select a.*, b.municipio as municipio_nome, c.descricao as bairro_nome from metas a left join municipios b on a.municipio = b.codigo left join bairros_comunidades c on a.bairro_comunidade = c.codigo where a.usuario = '{$_POST['usuario']}' and a.usuario > 0 order by municipio_nome, bairro_nome";
 $r = mysqli_query($con, $q);
 while($d = mysqli_fetch_object($r)){
 ?>
