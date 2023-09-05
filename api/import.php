66fc9ac89f5fdf80fb8c46bce12054095512b15c
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-09-06 01:55:46 +0300

    Atualizações

--- api/import.php
+++ api/import.php
@@ -69,7 +69,7 @@
             // if(is_array($value)) {
             //     $value = json_encode($value,JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
             // }
-            $qt = ((trim($value) and != '[""]')?($qt+1):$qt);
+            $qt = ((trim($value) and $value != '[""]')?($qt+1):$qt);
         }
             $pct = (100*$qt/$tot);
             $campos[] = "percentual = '{$pct}'";
