fa58c675a790ca6e0addb8b659efe480aa9408e1
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-09-06 14:28:30 +0300

    Atualizações

--- pct.php
+++ pct.php
@@ -1,13 +1,13 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/app/projectSocioEconomico/lib/includes.php");
 
+    exit();
+
     $query = "select * from se where situacao = 'c'";
     $result = mysqli_query($con, $query);
     while($d = mysqli_fetch_object($result)){
         // var_dump($d);
         // echo "<hr>";
-
-
         $tot = 0;
         $qt = 0;
         $remov = ['[""]', 'null', '0', '0.00', ' ', 0, null];
@@ -18,8 +18,8 @@
         }
 
 
-            $pct = (100*$qt/$tot);
-            echo $update = "update se set percentual = '" . $pct . "' where codigo = '{$d->codigo}'";
-            mysqli_query($con, $update);
-            echo "<hr>";
+        $pct = (100*$qt/$tot);
+        echo $update = "update se set percentual = '" . $pct . "' where codigo = '{$d->codigo}'";
+        mysqli_query($con, $update);
+        echo "<hr>";
     }
