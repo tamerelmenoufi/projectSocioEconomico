49e2b384c76aa06bb176dc40fe5d0e76359a8a2a
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-09-06 14:23:29 +0300

    Atualizações

--- pct.php
+++ pct.php
@@ -4,6 +4,21 @@
     $query = "select * from se where situacao = 'c'";
     $result = mysqli_query($con, $query);
     while($d = mysqli_fetch_object($result)){
-        var_dump($d);
-        echo "<hr>";
+        // var_dump($d);
+        // echo "<hr>";
+
+
+        $tot = 0;
+        $qt = 0;
+        $remov = ['[""]', 'null', '0', '0.00', ' ', 0, null];
+        $log = false;
+        foreach ($d as $name => $value) {
+            $qt = ((trim(str_replace($remov, false,$value)))?($qt+1):$qt);
+            $tot++;
+        }
+
+
+            $pct = (100*$qt/$tot);
+            echo $update = "update se set percentual = '" . $pct . "' where codigo = '{$d->codigo}'";
+            echo "<hr>";
     }
