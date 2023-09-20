bd8d67cdd44eb1d478b17a99e828d043d0b3bb73
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-09-21 01:54:18 +0300

    Atualizações

--- api/exportSqlPerfil.php
+++ api/exportSqlPerfil.php
@@ -4,7 +4,7 @@
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
     $_POST = json_decode(file_get_contents('php://input'), true);
 
-    $_POST['perfil'][0]['codigo'] = 248;
+    // $_POST['perfil'][0]['codigo'] = 248;
 
 
     $q = "select * from metas where data >= DATE_ADD(NOW(), INTERVAL -7 DAY) and data <= NOW() and usuario = '{$_POST['perfil'][0]['codigo']}' and situacao = '1' and deletado != '1'";
