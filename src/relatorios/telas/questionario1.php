14eb3400932f3381a019d7af578d497dc06b2211
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-09-21 16:38:50 +0300

    Atualizações

--- src/relatorios/telas/questionario1.php
+++ src/relatorios/telas/questionario1.php
@@ -289,7 +289,7 @@
                 },
                 success:function(dados){
                     // $.dialog(dados)
-                    obj.parent("li").removeClass("list-group-item-action list-group-item-light d-flex justify-content-between align-items-center");
+                    obj.parent("li").attr("class","");
                     obj.parent("li").html(dados);
                     Carregando('none');
                 }
