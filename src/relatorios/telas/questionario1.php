e971ea22db280b3318958eb1d908c9c2ac1beb7c
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-09-21 13:52:39 +0300

    Atualizações

--- src/relatorios/telas/questionario1.php
+++ src/relatorios/telas/questionario1.php
@@ -10,7 +10,7 @@
         
 ?>
 
-    <li class="list-group-item d-flex justify-content-between align-items-center">
+    <li class="list-group-item list-group-item-action list-group-item-light d-flex justify-content-between align-items-center">
         <div><i class="fa-solid fa-angle-right"></i> <?=$d['rotulo']?></div>
         <span class="badge bg-primary rounded-pill"><i class="fa-solid fa-chart-line"></i> Visualizar</span>
     </li>
