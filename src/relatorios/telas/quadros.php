9461b5b21a9667246bbf0858ab2b05dabd2bec48
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-09-21 14:50:26 +0300

    Atualizações

--- src/relatorios/telas/quadros.php
+++ src/relatorios/telas/quadros.php
@@ -41,7 +41,7 @@
                 if($J){
                     foreach($J as $i => $v){
                         
-                        $d->legenda->$i = ((trim($v))?:'Não Informado');
+                        $d->legenda->$v = ((trim($v))?:'Não Informado');
 
                         $D[$v] = ($D[$v] + 1);
                         $t = ($t + 1);
