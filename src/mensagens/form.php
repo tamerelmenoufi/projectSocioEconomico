a4a6231ca632cac1faabb774097b77ed284e48b4
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-09-21 08:56:54 +0300

    Atualizações

---
+++ src/mensagens/form.php
@@ -0,0 +1,139 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/app/projectSocioEconomico/lib/includes.php");
+
+
+    if($_POST['acao'] == 'salvar'){
+
+        $data = $_POST;
+        $attr = [];
+
+        unset($data['codigo']);
+        unset($data['acao']);
+
+        foreach ($data as $name => $value) {
+            $attr[] = "{$name} = '" . mysqli_real_escape_string($con, $value) . "'";
+        }
+
+        if(!$_POST['codigo']){
+            $attr[] = "data = NOW()";
+        }
+
+        $attr = implode(', ', $attr);
+
+        if($_POST['codigo']){
+            $query = "update mensagens set {$attr} where codigo = '{$_POST['codigo']}'";
+            mysqli_query($con, $query);
+            $cod = $_POST['codigo'];
+        }else{
+            $query = "insert into mensagens set {$attr}";
+            mysqli_query($con, $query);
+            $cod = mysqli_insert_id($con);
+        }
+
+        $retorno = [
+            'status' => true,
+            'codigo' => $cod
+        ];
+
+        echo json_encode($retorno);
+
+        exit();
+    }
+
+
+    $query = "select * from mensagens where codigo = '{$_POST['cod']}'";
+    $result = mysqli_query($con, $query);
+    $d = mysqli_fetch_object($result);
+?>
+<style>
+    .Titulo<?=$md5?>{
+        position:absolute;
+        left:60px;
+        top:8px;
+        z-index:0;
+    }
+</style>
+<h4 class="Titulo<?=$md5?>">Cadastro de Mensagens</h4>
+    <form id="form-<?= $md5 ?>">
+        <div class="row">
+            <div class="col">
+                <div class="form-floating mb-3">
+                    <input required type="text" class="form-control" id="titulo" name="titulo" placeholder="Título da Mensagem" value="<?=$d->titulo?>">
+                    <label for="titulo">Título*</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <textarea required name="mensagem" id="mensagem" class="form-control" placeholder="Mensagem"><?=$d->mensagem?>"</textarea>
+                    <label for="cor">Digite a seua mensagem*</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <select name="situacao" class="form-control" id="situacao">
+                        <option value="1" <?=(($d->situacao == '1')?'selected':false)?>>Liberado</option>
+                        <option value="0" <?=(($d->situacao == '0')?'selected':false)?>>Bloqueado</option>
+                    </select>
+                    <label for="email">Situação</label>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <div style="display:flex; justify-content:end">
+                    <button type="submit" class="btn btn-success btn-ms">Salvar</button>
+                    <input type="hidden" id="codigo" value="<?=$_POST['cod']?>" />
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <script>
+        $(function(){
+            Carregando('none');
+
+            
+            $('#form-<?=$md5?>').submit(function (e) {
+
+                e.preventDefault();
+
+                var codigo = $('#codigo').val();
+                var campos = $(this).serializeArray();
+
+                if (codigo) {
+                    campos.push({name: 'codigo', value: codigo})
+                }
+
+                campos.push({name: 'acao', value: 'salvar'})
+
+                Carregando();
+
+                $.ajax({
+                    url:"src/mensagens/form.php",
+                    type:"POST",
+                    typeData:"JSON",
+                    mimeType: 'multipart/form-data',
+                    data: campos,
+                    success:function(dados){
+                        // if(dados.status){
+                            $.ajax({
+                                url:"src/mensagens/index.php",
+                                type:"POST",
+                                success:function(dados){
+                                    $("#paginaHome").html(dados);
+                                    let myOffCanvas = document.getElementById('offcanvasDireita');
+                                    let openedCanvas = bootstrap.Offcanvas.getInstance(myOffCanvas);
+                                    openedCanvas.hide();
+                                }
+                            });
+                        // }
+                    },
+                    error:function(erro){
+
+                        // $.alert('Ocorreu um erro!' + erro.toString());
+                        //dados de teste
+                    }
+                });
+
+            });
+
+        })
+    </script>
---
+++ src/mensagens/index.php
@@ -0,0 +1,180 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/app/projectSocioEconomico/lib/includes.php");
+
+
+    if($_POST['delete']){
+      // $query = "delete from mensagens where codigo = '{$_POST['delete']}'";
+      $query = "update mensagens set deletado = '1' where codigo = '{$_POST['delete']}'";
+      mysqli_query($con, $query);
+    }
+
+    if($_POST['situacao']){
+      $query = "update mensagens set situacao = '{$_POST['opc']}' where codigo = '{$_POST['situacao']}'";
+      mysqli_query($con, $query);
+      exit();
+    }
+
+  
+?>
+
+<div class="col">
+  <div class="m-3">
+
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <h5 class="card-header">Lista de mensagens</h5>
+          <div class="card-body">
+            <div class="d-flex justify-content-end">
+                
+
+                <button
+                    novoCadastro
+                    class="btn btn-success"
+                    data-bs-toggle="offcanvas"
+                    href="#offcanvasDireita"
+                    role="button"
+                    aria-controls="offcanvasDireita"
+                    style="margin-left:20px;"
+                >Novo</button>
+            </div>
+
+
+            <table class="table table-striped table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Título</th>
+                  <th scope="col">Data</th>
+                  <th scope="col">Situação</th>
+                  <th scope="col">Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                  $query = "select a.* from mensagens a where a.deletado != '1' order by a.data desc";
+                  $result = mysqli_query($con, $query);
+                  while($d = mysqli_fetch_object($result)){
+                ?>
+                <tr>
+                  <td><?=$d->titulo?></td>
+                  <td><?=dataBr($d->data)?></td>
+                  <td>
+
+                  <div class="form-check form-switch">
+                    <input class="form-check-input situacao" type="checkbox" <?=(($d->situacao)?'checked':false)?> usuario="<?=$d->codigo?>">
+                  </div>
+
+                  </td>
+                  <td>
+                    <button
+                      class="btn btn-primary"
+                      edit="<?=$d->codigo?>"
+                      data-bs-toggle="offcanvas"
+                      href="#offcanvasDireita"
+                      role="button"
+                      aria-controls="offcanvasDireita"
+                    >
+                      Editar
+                    </button>
+                    <button class="btn btn-danger" delete="<?=$d->codigo?>">
+                      Excluir
+                    </button>
+                  </td>
+                </tr>
+                <?php
+                  }
+                ?>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+<script>
+    $(function(){
+        Carregando('none');
+
+        $("button[novoCadastro]").click(function(){
+            $.ajax({
+                url:"src/mensagens/form.php",
+                success:function(dados){
+                    $(".LateralDireita").html(dados);
+                }
+            })
+        })
+
+        $("button[edit]").click(function(){
+            cod = $(this).attr("edit");
+            $.ajax({
+                url:"src/mensagens/form.php",
+                type:"POST",
+                data:{
+                  cod
+                },
+                success:function(dados){
+                    $(".LateralDireita").html(dados);
+                }
+            })
+        })
+
+        
+        $("button[delete]").click(function(){
+            deletar = $(this).attr("delete");
+            $.confirm({
+                content:"Deseja realmente excluir o cadastro ?",
+                title:false,
+                buttons:{
+                    'SIM':function(){
+                        $.ajax({
+                            url:"src/mensagens/index.php",
+                            type:"POST",
+                            data:{
+                                delete:deletar
+                            },
+                            success:function(dados){
+                                $("#paginaHome").html(dados);
+                            }
+                        })
+                    },
+                    'NÃO':function(){
+
+                    }
+                }
+            });
+
+        })
+
+
+        $(".situacao").change(function(){
+
+            situacao = $(this).attr("usuario");
+            opc = false;
+
+            if($(this).prop("checked") == true){
+              opc = '1';
+            }else{
+              opc = '0';
+            }
+
+
+            $.ajax({
+                url:"src/mensagens/index.php",
+                type:"POST",
+                data:{
+                    situacao,
+                    opc
+                },
+                success:function(dados){
+                    // $("#paginaHome").html(dados);
+                }
+            })
+
+        });
+
+    })
+</script>
