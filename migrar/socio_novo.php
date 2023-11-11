b3c4fe5ff467fffbd61ff87e10eb107757789121
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-11-11 02:57:20 +0200

    atualizações

--- migrar/socio_novo.php
+++ migrar/socio_novo.php
@@ -2,7 +2,7 @@
 
     $con = mysqli_connect("project.mohatron.com","root","SenhaDoBanco", "app");
     
-    $query = "select a.codigo, (select cpf from mapcenso_novo where cpf = a.cpf limit 1 ) as tem from se a where a.acao_relatorio = '0' limit 10";
+    $query = "select a.codigo, (select cpf from mapcenso_novo where cpf = a.cpf limit 1 ) as tem from se a where a.acao_relatorio = '0' limit 50";
     $result = mysqli_query($con, $query);
     $r = [];
     while($d = mysqli_fetch_object($result)){
