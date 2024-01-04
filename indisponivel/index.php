9dfb0110bf768886860476713177b1c7f86e0c1b
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-01-04 10:27:12 +0200

    atualizações

---
+++ indisponivel/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Indisponível</title>
+    <style>
+        body{
+            background:url(img/bg.jpg)
+        }
+    </style>
+</head>
+<body>
+    <p style="text-align:center; margin-top:50px; width:100%">
+        <img src="img/indisponivel.jpg" alt="Sistema Indisponível">
+    </p>
+</body>
+</html>
